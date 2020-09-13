150c1bdb005f05f99f4ef630a6001e0e209ec770
DarthPointer <al.kiriackin@yandex.ru>
2020-09-13 21:09:14 +0300

    Going 0.0

---
+++ Source/ProbeControlUnlimiter/ProbeControlUnlimiter/ModuleCommandProbeUnlimited.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using CommNet;
+
+namespace ProbeControlUnlimiter
+{
+    public class ProbeCMDUnlimiter : PartModule, ICommNetControlSource
+    {
+        List<ModuleCommand> commandModules;
+        public override void OnStart(StartState state)
+        {
+            commandModules = part.FindModulesImplementing<ModuleCommand>();
+        }
+
+        public void UpdateNetwork() { }
+
+        public VesselControlState GetControlSourceState()
+        {
+            foreach (ModuleCommand moduleCommand in commandModules)
+            {
+                if (moduleCommand.GetControlSourceState() == VesselControlState.ProbePartial)
+                {
+                    return VesselControlState.ProbeFull;
+                }
+            }
+
+            return VesselControlState.None;
+        }
+
+        public bool IsCommCapable() { return false; }
+    }
+}
