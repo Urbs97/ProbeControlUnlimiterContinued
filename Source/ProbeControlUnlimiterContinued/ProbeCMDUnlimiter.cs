ee35f2a115d7512fdf9c8ade2e5f4e37080da47b
Urbs <urbs97@pm.me>
2023-03-21 00:10:24 +0100

    Cleanup + Refactoring

---
+++ Source/ProbeControlUnlimiterContinued/ProbeCMDUnlimiter.cs
@@ -0,0 +1,33 @@
+﻿using CommNet;
+using System.Collections.Generic;
+
+namespace ProbeControlUnlimiter
+{
+    public class ProbeCMDUnlimiter : PartModule, ICommNetControlSource
+    {
+        List<ModuleCommand> commandModules = new List<ModuleCommand>();
+
+        public override void OnStart(StartState state)
+        {
+            commandModules = part.FindModulesImplementing<ModuleCommand>();
+        }
+
+        public void UpdateNetwork() { }
+
+        public VesselControlState GetControlSourceState()
+        {
+            // not using LINQ because of performance concerns
+            foreach (ModuleCommand moduleCommand in commandModules)
+            {
+                if (moduleCommand.GetControlSourceState() is VesselControlState.ProbePartial)
+                {
+                    return VesselControlState.ProbeFull;
+                }
+            }
+
+            return VesselControlState.None;
+        }
+
+        public bool IsCommCapable() => false;
+    }
+}
---
+++ Source/ProbeControlUnlimiterContinued/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProbeControlUnlimiterContinued")]
+[assembly: AssemblyDescription("This is a KSP mod that provides full contol for probes without signal while commnet is not disabled.")]
+[assembly: AssemblyCompany("Urbs97")]
+[assembly: AssemblyProduct("ProbeControlUnlimiterContinued")]
+[assembly: AssemblyCopyright("Copyright©2023")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
